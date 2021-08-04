8a13123bcb56221ca7d4c616cf3050e286211742
SuperGibbon <super_gibbon@hotmail.com>
2021-08-04 15:55:06 +0000

    Update admin

--- guides/admin.py
+++ guides/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Guide, Category
 
 
-admin.site.register(Guide)
-admin.site.register(Category)
+class GuideAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Guide, GuideAdmin)
+admin.site.register(Category, CategoryAdmin)
