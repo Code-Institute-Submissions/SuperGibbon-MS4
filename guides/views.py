c9237aa3fe33867e1d951976240e8271164771ff
SuperGibbon <super_gibbon@hotmail.com>
2021-08-04 16:08:18 +0000

    Add guide view and template

--- guides/views.py
+++ guides/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Guide
 
 # Create your views here.
+
+
+def all_guides(request):
+    """ view to return all guides page """
+
+    guides = Guide.objects.all()
+
+    context = {
+        'guides': guides,
+    }
+
+    return render(request, 'guides/guides.html', context)
