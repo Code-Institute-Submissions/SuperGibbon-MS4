fb91794dbc2ebb9d3bb345d79218968dbdcf7248
SuperGibbon <super_gibbon@hotmail.com>
2021-08-05 14:04:37 +0000

    Add search functionality

--- guides/views.py
+++ guides/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Guide
 
 # Create your views here.
@@ -8,9 +10,22 @@ def all_guides(request):
     """ view to return all guides page """
 
     guides = Guide.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,
+                               "You didn't enter anything in the search box")
+                return redirect(reverse('guides'))
+
+            queries = Q(name__icontains=query) | Q(desciption__icontains=query)
+            guides = guides.filter(queries)
 
     context = {
         'guides': guides,
+        'search_term': query,
     }
 
     return render(request, 'guides/guides.html', context)
