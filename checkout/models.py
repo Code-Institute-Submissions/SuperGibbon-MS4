0fda735244736c023f3c1dfc59c7cc41f8bca765
SuperGibbon <super_gibbon@hotmail.com>
2021-08-06 08:56:09 +0000

    Add checkout app and models

---
+++ checkout/models.py
@@ -0,0 +1,24 @@
+import uuid
+
+from django.db import models
+from django.conf import settings
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                        null=False, default=0)
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
