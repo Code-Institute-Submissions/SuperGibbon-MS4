2a9c4fc5cc0b0ecd3540d3dc48d55daebc785aa6
SuperGibbon <super_gibbon@hotmail.com>
2021-08-09 08:30:57 +0000

    Added fixtures for db to use for deployment to heroku

--- gibbon_guides/settings.py
+++ gibbon_guides/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,11 +120,15 @@ WSGI_APPLICATION = 'gibbon_guides.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://bmktarlodievon:c51ec55e7b3c8deb8b8576c3c2ca2608461fd48b9e39b97bfc41707af262d5d1@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/d9691u7fnubfpd')
 }
 
 
